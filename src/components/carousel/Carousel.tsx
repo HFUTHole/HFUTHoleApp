b70cbb0939a1402ffd7b9f9c8570513cbfe64cb2
Quartz68 <wangzimeng2004@gmail.com>
2023-08-04 04:37:07 +0800

    Added snap to carousel

--- src/components/carousel/Carousel.tsx
+++ src/components/carousel/Carousel.tsx
@@ -1,6 +1,13 @@
 import CarouselItem from './CarouselItem'
 import { useSharedValue } from 'react-native-reanimated'
 import { FlatList as GestureHandlerFlatList } from 'react-native-gesture-handler'
+import { Dimensions } from 'react-native'
+
+const { width } = Dimensions.get('window')
+
+const LARGE_IMAGE_WIDTH = width * 0.45
+const MEDIUM_IMAGE_WIDTH = LARGE_IMAGE_WIDTH * 0.75
+const SMALL_IMAGE_WIDTH = MEDIUM_IMAGE_WIDTH * 0.34
 
 export function Carousel({ data }) {
   const scrollX = useSharedValue(0)
@@ -19,6 +26,9 @@ export function Carousel({ data }) {
       onScroll={scrollHandler}
       overScrollMode="never"
       scrollEventThrottle={16}
+      snapToAlignment={'start'}
+      snapToInterval={SMALL_IMAGE_WIDTH}
+      decelerationRate={'fast'}
       keyExtractor={(item) => item.id.toString()}
       renderItem={({ item, index }) => (
         <CarouselItem
