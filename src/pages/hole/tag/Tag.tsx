0d87e7aabaa1d0b45053e2c83be2f7726edfc1ea
snowingfox <1503401882@qq.com>
2024-05-12 22:16:09 +0800

    feat: change to yarn

--- src/pages/hole/tag/Tag.tsx
+++ src/pages/hole/tag/Tag.tsx
@@ -17,54 +17,55 @@ import { RefreshableHoleList } from '@/pages/hole/components/HoleList'
 import { useParams } from '@/shared/hooks/useParams'
 import { useTagParams } from '@/pages/hole/tag/useTagParams'
 
-function PostList() {
+function PostHeader() {
   const { tag } = useTagParams()
 
-  const query = useTagPostList(tag)
+  const { data, isLoading } = useTagDetailSwr(tag)
 
   return (
-    <ScrollView>
-      <View className="flex flex-row space-x-2 px-2">
-        <RefreshableHoleList
-          {...query}
-          contentContainerStyle={{
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            justifyContent: 'center',
-            gap: 5,
-          }}
-          renderItem={({ item: data }) => {
-            const item = data as unknown as IHole
-
-            return (
-              <View className={'w-[47vw]'}>
-                <TagHoleInfo data={data} />
-              </View>
-            )
-          }}
-        />
+    <>
+      <View className="px-4 py-4">
+        <Text variant="headlineSmall">{`# ${tag}`}</Text>
+        <View className="flex justify-between items-center flex-row">
+          <Text>{`${data?.views}浏览`}</Text>
+        </View>
       </View>
-    </ScrollView>
+      <View className="h-[1px] bg-gray-400/40 mb-2"></View>
+    </>
   )
 }
 
 export function TagScreen() {
   const { tag } = useTagParams()
 
-  const { data, isLoading } = useTagDetailSwr(tag)
+  const query = useTagPostList(tag)
 
   return (
-    <LoadingScreen isLoading={isLoading}>
+    <LoadingScreen isLoading={query.isLoading}>
       <SafeAreaView className="flex-1 w-full bg-white">
         <BackHeader title={'话题'} />
-        <View className="px-4 py-4">
-          <Text variant="headlineSmall">{`# ${tag}`}</Text>
-          <View className="flex justify-between items-center flex-row">
-            <Text>{`${data?.views}浏览`}</Text>
-          </View>
-        </View>
-        <View className="h-[1px] bg-gray-400/40 mb-2"></View>
-        <PostList />
+
+        <ScrollView>
+          <PostHeader />
+          <RefreshableHoleList
+            {...query}
+            contentContainerStyle={{
+              flexDirection: 'row',
+              flexWrap: 'wrap',
+              justifyContent: 'center',
+              gap: 5,
+            }}
+            renderItem={({ item: data }) => {
+              const item = data as unknown as IHole
+
+              return (
+                <View className={'w-[47vw]'}>
+                  <TagHoleInfo data={data} />
+                </View>
+              )
+            }}
+          />
+        </ScrollView>
       </SafeAreaView>
     </LoadingScreen>
   )
